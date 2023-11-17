4d09bdd31933fa01308e4e8ca2c7a2edb69f0e9d
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-17 14:18:29 -0500

    #Update: deleted data inside assets and move it in constants

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -8,5 +8,8 @@ import icon02 from '../assets/images/icon02.png';
 import icon03 from '../assets/images/icon03.png';
 import aboutImg from '../assets/images/about.png';
 import aboutCardImg from '../assets/images/about-card.png';
+import doctorImg01 from "../assets/images/doctor-img01.png";
+import doctorImg02 from "../assets/images/doctor-img02.png";
+import doctorImg03 from "../assets/images/doctor-img03.png";
 
-export { heroImg01, heroImg02, heroImg03, logo, userImg, icon01, icon02, icon03, aboutImg, aboutCardImg };
+export { heroImg01, heroImg02, heroImg03, logo, userImg, icon01, icon02, icon03, aboutImg, aboutCardImg, doctorImg01, doctorImg02, doctorImg03 };
--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,4 @@
-import { icon01, icon02, icon03 } from "../assets"
+import { icon01, icon02, icon03, doctorImg01, doctorImg02, doctorImg03 } from "../assets"
 
 export const navLinks = [
     {
@@ -38,4 +38,106 @@ export const finderLinks = [
         icon: icon03,
         description: 'World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic.'
     }
-]
+];
+
+export const services = [
+    {
+      name: "Cancer Care",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(254, 182, 13, .2)",
+      textColor: "#FEB60D",
+    },
+    {
+      name: "Labor & Delivery",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
+    },
+    {
+      name: "Heart & Vascular",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(1, 181, 197, .2)",
+      textColor: "#01B5C5",
+    },
+    {
+      name: "Mental Health",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(1, 181, 197, .2)",
+      textColor: "#01B5C5",
+    },
+    {
+      name: "Neurology",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(254, 182, 13, .2)",
+      textColor: "#FEB60D",
+    },
+    {
+      name: "Burn Treatment",
+      desc: "World-class care for everyone. Our health System offers unmatched, expert health care. From the lab to the clinic.",
+      bgColor: "rgba(151, 113, 255, .2)",
+      textColor: "#9771FF",
+    },
+];
+
+export const doctors = [
+    {
+      id: "01",
+      name: "Dr. Romel Cagurangan",
+      specialty: "Surgeon",
+      avgRating: 4.8,
+      totalRating: 272,
+      photo: doctorImg01,
+      totalPatients: 1500,
+      hospital: "Mount Adora Hospital, Sylhet.",
+    },
+    {
+      id: "02",
+      name: "Dr. Jackelyn Trinidad",
+      specialty: "Neurologist",
+      avgRating: 4.8,
+      totalRating: 272,
+      photo: doctorImg02,
+      totalPatients: 1500,
+      hospital: "Mount Adora Hospital, Sylhet.",
+    },
+    {
+      id: "03",
+      name: "Dr. John Brain Dugay",
+      specialty: "Dermatologist",
+      avgRating: 4.8,
+      totalRating: 272,
+      photo: doctorImg03,
+      totalPatients: 1500,
+      hospital: "Mount Adora Hospital, Sylhet.",
+    },
+  ];
+ 
+
+export const faqs = [
+    {
+      question: "What is your medical care?",
+      content:
+        "One Medical was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
+    },
+    {
+      question: "What happens if I need to go a hospital?",
+      content:
+        "One Medical was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
+    },
+    {
+      question: "What happens if I need to go a hospital?",
+      content:
+        "One Medical was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
+    },
+    {
+      question: "Can I visit your medical office?",
+      content:
+        "One Medical was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
+    },
+    {
+      question: "Does you provide urgent care?",
+      content:
+        "One Medical was founded on a better model of care one designed around patients needs that provides a higher level of quality and service affordably. We do this through innovative design, excellent customer service, and the efficient use of technology.",
+    },
+  ];
+  
