c6528e39c14e4c79463b0b31daca7d8e0ec9cb71
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-11-16 22:56:26 -0500

    #Add: constants values to be used across the application

---
+++ client/src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        path:'/home',
+        display: 'Home'
+    },
+    {
+        path:'/doctors',
+        display: 'Find a Doctor'
+    },
+    {
+        path:'/services',
+        display: 'Services'
+    },
+    {
+        path:'/contact',
+        display: 'Contact'
+    },
+]
