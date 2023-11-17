d52d710df4bdf204394457089a7b11789fba07a8
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-17 01:48:42 -0500

    #Update: added finderLinks for grid items

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,3 +1,5 @@
+import { icon01, icon02, icon03 } from "../assets"
+
 export const navLinks = [
     {
         path:'/home',
@@ -15,4 +17,25 @@ export const navLinks = [
         path:'/contact',
         display: 'Contact'
     },
+];
+
+export const finderLinks = [
+    {
+        path: '/doctors',
+        title: 'Find a Doctor',
+        icon: icon01,
+        description: 'World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic.'
+    },
+    {
+        path: '/doctors',
+        title: 'Find a Location',
+        icon: icon02,
+        description: 'World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic.'
+    },
+    {
+        path: '/doctors',
+        title: 'Book Appointment',
+        icon: icon03,
+        description: 'World-class care for everyone. Our health system offers unmatched, expert health care. From the lab to the clinic.'
+    }
 ]
