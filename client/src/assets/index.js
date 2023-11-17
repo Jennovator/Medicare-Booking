0a069588fa50b4baa6b787e05d242419d5ff6ac4
Jennovator <jenylltrinidadmabborang@gmail.com>
2023-11-16 23:55:31 -0500

    Update: basic info and styles for Home page. Added index.jsfor exporting assets

---
+++ client/src/assets/index.js
@@ -0,0 +1,7 @@
+import heroImg01 from '../assets/images/hero-img01.png';
+import heroImg02 from '../assets/images/hero-img02.png';
+import heroImg03 from '../assets/images/hero-img03.png';
+import logo from '../assets/images/logo.png';
+import userImg from '../assets/images/avatar-icon.png';
+
+export { heroImg01, heroImg02, heroImg03, logo, userImg };
