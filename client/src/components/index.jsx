7511d77ddb40bc3e40341929189d12f7fefe1be1
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-20 15:33:58 -0500

    #Update: added faqs components for faq's page

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,9 +1,15 @@
 import Header from "./Header/Header";
 import About from "./About/About";
 import Footer from "./Footer/Footer";
+import ServiceList from "./Services/ServiceList";
+import DoctorList from "./Doctors/DoctorList";
+import FaqList from "./Faq/FaqList";
 
 export {
     Header,
     About,
+    ServiceList,
+    DoctorList,
+    FaqList,
     Footer
 };
