8e2fbd3fe648d771ca88fa5bde43fd8cbb5ccd93
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-20 17:06:51 -0500

    #Update: added footer content

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React from 'react'; 
+import { Link } from 'react-router-dom';
+import { logo } from '../../assets';
+import { socialLinks, quickLinks01, quickLinks02, quickLinks03 } from '../../constants';
 
 const Footer = () => {
+
+  const year = new Date().getFullYear();
+
   return (
-    <div>Footer</div>
+    <footer>
+      <div className="container">
+        <div>
+          <div>
+            <img src={logo} alt='Medicare'/>
+            <p>
+              Copyright © {year} developed by Jenyll Mabborang all right reserved.
+            </p>
+
+            <div>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
