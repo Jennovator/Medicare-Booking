2a778a74a395246f22d31d2cd341a6eb1d5346b1
Jennovator <139597511+Jennovator@users.noreply.github.com>
2023-11-21 12:52:20 -0500

    #Update: added content for service page

--- client/src/pages/Services.jsx
+++ client/src/pages/Services.jsx
@@ -1,8 +1,20 @@
 import React from 'react';
+import { services } from '../constants';
+import ServiceCard from '../components/Services/ServiceCard';
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section>
+      <div className="container">
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]'>
+          {
+            services.map((item, index) => (
+              <ServiceCard item={item} index={index} key={index} />
+            ))
+          }
+        </div>
+      </div>
+    </section>
   )
 }
 
